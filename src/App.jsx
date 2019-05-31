8ef1f1ee06749e6aaff9e1a5a002eb7b6ba3c5f1
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-05-30 22:13:55 -0500

    ran Eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React, { PureComponent } from "react";
-import "./App.css";
+import React, { PureComponent } from 'react';
+import './App.css';
 
 export class App extends PureComponent {
   render() {
