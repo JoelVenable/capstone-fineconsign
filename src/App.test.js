b1a6bce53638c9e913267652aeba4d729bb1a400
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-05-30 21:25:58 -0500

    fixed prettier to only check js and jsx files

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,6 @@ import React from "react";
 import { shallow } from "enzyme";
 import { App } from "./App";
 
-
 describe("<App />", () => {
   it("renders <App /> without crashing", () => {
     const wrapper = shallow(<App />);
