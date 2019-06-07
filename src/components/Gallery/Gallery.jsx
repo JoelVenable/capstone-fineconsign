9efc83288454520e4c7d91818aa0c4d04c146fce
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-06-07 09:57:44 -0500

    Added Semantic UI framework

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react';
+
 
 export class Gallery extends Component {
   render() {
     return (
-      <div>Hello from Gallery!</div>
+      <>
+        <div>Hello from Gallery!</div>
+        <Button primary>Click Here</Button>
+      </>
     );
   }
 }
