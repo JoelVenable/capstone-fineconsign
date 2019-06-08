5c6943bcd41ac54a17c6ed9f33c1a8bf056c68ca
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-06-08 08:31:04 -0500

    Moved route variables to separate file.

--- src/ApplicationViews.jsx
+++ src/ApplicationViews.jsx
@@ -1,22 +1,14 @@
 import React, { PureComponent } from 'react';
 import { Route } from 'react-router-dom';
-import { Welcome } from './components/welcome/Welcome';
 import { ProtectedRoute } from './components/utility/ProtectedRoute';
-import { Users } from './components/Users/Users';
-import { Appraisals } from './components/Appraisals/Appraisals';
-import { Approvals } from './components/Approvals/Approvals';
-import { Gallery } from './components/Gallery/Gallery';
-import { Customers } from './components/Customers/Customers';
-import { Orders } from './components/Orders/Orders';
-import { PriceAdjustments } from './components/PriceAdjustments/PriceAdjustments';
-import { Stores } from './components/Stores/Stores';
 import { UserConsumer } from './Context/UserContextProvider';
+import { routes, protectedRoutes } from './routes';
 
 
 export class ApplicationViews extends PureComponent {
-  makeProtectedRoutes = routes => routes.map(route => <ProtectedRoute {...route} />)
+  makeProtectedRoutes = myRoutes => myRoutes.map(route => <ProtectedRoute {...route} key={route.path} />)
 
-  makeClearRoutes = routes => routes.map(route => <Route {...route} />)
+  makeClearRoutes = myRoutes => myRoutes.map(route => <Route {...route} key={route.path} />)
 
   render() {
     return (
@@ -25,64 +17,9 @@ export class ApplicationViews extends PureComponent {
           {user => (('id' in user) ? <div className="hello" /> : null)}
         </UserConsumer>
 
-        {this.makeClearRoutes(this.routes)}
-        {this.makeProtectedRoutes(this.protectedRoutes)}
+        {this.makeClearRoutes(routes)}
+        {this.makeProtectedRoutes(protectedRoutes)}
       </>
     );
   }
-
-
-  protectedRoutes = [
-    {
-      path: '/users',
-      render: props => <Users {...props} />,
-      isAuthorized: true,
-      exact: true,
-    },
-    {
-      path: '/appraisals',
-      render: props => <Appraisals {...props} />,
-      isAuthorized: true,
-      exact: true,
-    },
-    {
-      path: '/approvals',
-      render: props => <Approvals {...props} />,
-      isAuthorized: true,
-      exact: true,
-    }, {
-      path: '/customers',
-      render: props => <Customers {...props} />,
-      isAuthorized: true,
-      exact: true,
-    }, {
-      path: '/orders',
-      render: props => <Orders {...props} />,
-      isAuthorized: true,
-      exact: true,
-    }, {
-      path: '/priceAdjustments',
-      render: props => <PriceAdjustments {...props} />,
-      isAuthorized: true,
-      exact: true,
-    }, {
-      path: '/stores',
-      render: props => <Stores {...props} />,
-      isAuthorized: true,
-      exact: true,
-    },
-  ]
-
-  routes = [
-    {
-      path: '/',
-      render: props => <Welcome {...props} />,
-      exact: true,
-    },
-    {
-      path: '/gallery',
-      render: props => <Gallery {...props} />,
-      exact: true,
-    },
-  ]
 }
---
+++ src/routes.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Users } from './components/Users/Users';
+import { Appraisals } from './components/Appraisals/Appraisals';
+import { Approvals } from './components/Approvals/Approvals';
+import { Gallery } from './components/Gallery/Gallery';
+import { Customers } from './components/Customers/Customers';
+import { Orders } from './components/Orders/Orders';
+import { PriceAdjustments } from './components/PriceAdjustments/PriceAdjustments';
+import { Stores } from './components/Stores/Stores';
+import { Welcome } from './components/welcome/Welcome';
+
+
+export const protectedRoutes = [
+  {
+    path: '/users',
+    render: props => <Users {...props} />,
+    isAuthorized: true,
+    exact: true,
+  }, {
+    path: '/appraisals',
+    render: props => <Appraisals {...props} />,
+    isAuthorized: true,
+    exact: true,
+  }, {
+    path: '/approvals',
+    render: props => <Approvals {...props} />,
+    isAuthorized: true,
+    exact: true,
+  }, {
+    path: '/customers',
+    render: props => <Customers {...props} />,
+    isAuthorized: true,
+    exact: true,
+  }, {
+    path: '/orders',
+    render: props => <Orders {...props} />,
+    isAuthorized: true,
+    exact: true,
+  }, {
+    path: '/priceAdjustments',
+    render: props => <PriceAdjustments {...props} />,
+    isAuthorized: true,
+    exact: true,
+  }, {
+    path: '/stores',
+    render: props => <Stores {...props} />,
+    isAuthorized: true,
+    exact: true,
+  },
+];
+
+export const routes = [
+  {
+    path: '/',
+    render: props => <Welcome {...props} />,
+    exact: true,
+  }, {
+    path: '/gallery',
+    render: props => <Gallery {...props} />,
+    exact: true,
+  },
+];
