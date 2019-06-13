8f3978f6f22bc4aea9c637658c57c312de5a1f6c
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-06-13 13:06:01 -0500

    Add painting works!!

--- src/components/PaintingList/AddPainting.jsx
+++ src/components/PaintingList/AddPainting.jsx
@@ -1,16 +1,72 @@
 import React, { useState } from 'react';
-import { Input, TextArea, Form } from 'semantic-ui-react';
+import {
+  Input, TextArea, Form, Button,
+} from 'semantic-ui-react';
 import './addPainting.css';
 import { Consumer } from '../../ContextProvider';
+import { compressImage } from '../utility/compressImage';
+
 
 export function AddPainting() {
   const [artistId, setArtistId] = useState(null);
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [photo, setPhoto] = useState(null);
+
+
+  async function handleSubmit(e, firebaseStorage, add) {
+    e.preventDefault();
+    const mainImg = await compressImage(photo, 'mainImg');
+    const thumbImg = await compressImage(photo, 'thumbImg');
+
+    add.painting({
+      name,
+      artistId,
+      description,
+      imgUrl: await firebaseStorage.child(`${Date.now()}-${name}-main`)
+        .put(mainImg)
+        .then(response => response.ref.getDownloadURL()),
+      thumbUrl: await firebaseStorage.child(`${Date.now()}-${name}-thumb`)
+        .put(thumbImg)
+        .then(response => response.ref.getDownloadURL()),
+    });
+  }
+
+  // saveProfile = () => {
+  //   const ref = this.storageRef.child(`${Date.now()}`);
+  //   ref
+  //     .put(this.state.photo)
+  //     .then(response => response.ref.getDownloadURL())
+  //     .then(url =>
+  //       postData({
+  //         username: this.state.username,
+  //         aboutMe: this.state.aboutMe,
+  //         photoUrl: url
+  //       })
+  //     );
+  // };
+
+  function handleChange(_e, { id, value }) {
+    console.log(id);
+    console.log(value);
+  }
+
 
   return (
-    <Form>
-      <Form.Field required control={Input} label="Painting Name" placeholder="Painting name" />
-      <Consumer>
-        {({ artists }) => (
+    <Consumer>
+      {({ storageRef, artists, add }) => (
+
+
+        <Form onSubmit={e => handleSubmit(e, storageRef, add)}>
+          <Form.Field
+            required
+            value={name}
+            onChange={(_e, { value }) => setName(value)}
+            control={Input}
+            label="Painting Name"
+            placeholder="Painting name"
+            id="name"
+          />
           <Form.Select
             label="Artist Name"
             required
@@ -20,15 +76,29 @@ export function AddPainting() {
               value: artist.id,
               image: { avatar: true, src: artist.artistImageUrl },
             }))}
-            onChange={setArtistId}
+            onChange={(_e, { value }) => setArtistId(value)}
+            value={artistId}
+            id="artistId"
           />
-        )
-        }
-      </Consumer>
 
-      <TextArea rows={7} placeholder="Painting description" />
+          <Form.Field
+            control="input"
+            type="file"
+            label="Photo"
+            onChange={e => setPhoto(e.target.files[0])}
+            placeholder="Upload an image"
+          />
 
+          <TextArea
+            value={description}
+            onChange={(_e, { value }) => setDescription(value)}
+            rows={7}
+            placeholder="Painting description"
+          />
 
-    </Form>
+          <Button type="submit" content="Save" color="purple" />
+        </Form>
+      )}
+    </Consumer>
   );
 }
