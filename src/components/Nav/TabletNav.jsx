3b913255868aed0a8f3f206ba59cf07daffd3502
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-06-13 10:57:15 -0500

    Navbar renders properly on tablets and computers

---
+++ src/components/Nav/TabletNav.jsx
@@ -0,0 +1,260 @@
+import React from 'react';
+import { Dropdown, Menu } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+import { Consumer } from '../../ContextProvider';
+
+export function TabletMenu({ user, history }) {
+  function handleClick(_e, { link }) {
+    history.push(link);
+  }
+  if (user) {
+    const { employee } = user;
+    switch (user.userType) {
+      case 'employee':
+        return (
+          <Menu.Menu position="right">
+
+            <Dropdown item floating button text="User Types">
+              <Dropdown.Menu>
+                {employee.canEditInventory
+                  ? (
+                    <Dropdown.Item
+                      key="key"
+                      icon="paint brush"
+                      text="Artists"
+                      value="value"
+                      link="/artists"
+                      onClick={handleClick}
+                    />
+                  ) : null}
+                {employee.canEditEmployees
+                  ? (
+                    <Dropdown.Item
+                      key="employees"
+                      icon="id card"
+                      text="Employees"
+                      value="employees"
+                      onClick={handleClick}
+                      link="/employees"
+                    />
+                  ) : null}
+                {employee.canEditCustomers
+                  ? (
+                    <Dropdown.Item
+                      key="customers"
+                      icon="users"
+                      text="Customers"
+                      onClick={handleClick}
+                      value="customers"
+                      link="/customers"
+                    />
+                  ) : null}
+                {employee.canEditEmployees
+                  ? (
+                    <Dropdown.Item
+                      key="users"
+                      onClick={handleClick}
+                      icon="wrench"
+                      text="Users"
+                      value="users"
+                      link="/users"
+                    />
+                  ) : null}
+              </Dropdown.Menu>
+            </Dropdown>
+            {employee.canEditInventory
+              ? (
+                <Menu.Item
+                  key="paintings"
+                  icon="file image"
+                  onClick={handleClick}
+                  name="Paintings"
+                  value="paintings"
+                  link="/paintings"
+                />
+              ) : null}
+            {employee.canProcessOrders
+              ? (
+                <Menu.Item
+                  key="orders"
+                  onClick={handleClick}
+                  icon="box"
+                  name="Orders"
+                  value="orders"
+                  link="/orders"
+                />
+              ) : null}
+
+
+            {employee.canDefinePriceAdjustments
+              ? (
+                <Menu.Item
+                  key="priceAdjustments"
+                  onClick={handleClick}
+                  icon="dollar sign"
+                  name="Price Adjustments"
+                  value="priceAdjustments"
+                  link="/priceAdjustments"
+                />
+              ) : null}
+            <SignOutButton />
+
+
+          </Menu.Menu>
+        );
+      case 'customer':
+        return (
+
+          <Menu.Menu position="right">
+            <Menu.Item
+              key="favorites"
+              onClick={handleClick}
+              icon="paint brush"
+              name="Favorite Artists"
+              value="artists"
+              link="/artists"
+            />
+            <Menu.Item
+              key="account"
+              onClick={handleClick}
+              icon="edit"
+              name="My Account"
+              value="account"
+              link="/account"
+            />
+            <Menu.Item
+              key="cart"
+              onClick={handleClick}
+              icon="shopping cart"
+              name="My Cart"
+              value="cart"
+              link="/cart"
+            />
+
+            <SignOutButton />
+          </Menu.Menu>
+        );
+      case 'artist':
+        return (
+          <Menu.Menu position="right">
+            <Menu.Item
+              key="paintings"
+              onClick={handleClick}
+              icon="file image"
+              name="My Paintings"
+              value="paintings"
+              link="/paintings"
+            />
+            <Menu.Item
+              key="account"
+              onClick={handleClick}
+              icon="edit"
+              name="My Account"
+              value="account"
+              link="/account"
+            />
+            <SignOutButton />
+
+          </Menu.Menu>
+        );
+    }
+  }
+
+  return (
+    <>
+      <Menu.Menu position="right">
+        <Dropdown item icon="shopping cart" />
+      </Menu.Menu>
+    </>
+  );
+}
+
+function SignOutButton() {
+  return (
+    <Consumer>
+      {({ logout }) => (
+        <Dropdown.Item
+          key="signout"
+          icon="sign-out"
+          text="Sign Out"
+          value="signout"
+          onClick={logout}
+        />
+      )
+    }
+    </Consumer>
+  );
+}
+
+
+TabletMenu.propTypes = {
+  user: PropTypes.shape({
+    userType: PropTypes.oneOf(['employee', 'artist', 'customer']).isRequired,
+    employee: PropTypes.shape({
+      canEditIventory: PropTypes.bool,
+      canDefinePriceAdjustments: PropTypes.bool,
+      canProcessOrders: PropTypes.bool,
+      canEditCustomers: PropTypes.bool,
+    }),
+  }),
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
+
+TabletMenu.defaultProps = {
+  user: null,
+};
+/*
+
+
+Icons:
+
+"file image"  -- Paintings
+"paint brush"   -- Artists
+"id card" --     Employees
+"box"  --  Orders
+"users" -- Customers
+"wrench"  -- Users
+"dollar sign" -- Price Adjustments
+ "sign-out"  -- Sign Out
+
+"edit" -- My account
+
+
+"shopping cart" -- Shopping Cart
+
+
+Employee options:
+
+Artists
+Paintings
+Employees
+Orders
+Customers
+Users
+Price Adjustments
+
+Sign Out
+
+
+Artist Options:
+
+My Paintings
+My Profile
+My Account
+
+Sign Out
+
+Customer Options:
+My Cart
+---
+My Orders
+My Account
+Favorite Artists
+
+Sign out
+
+
+*/
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,28 +3,29 @@ import { Menu, Button, Icon } from 'semantic-ui-react';
 import { Consumer } from '../ContextProvider';
 import { Title } from './welcome/Title';
 import './navbar.css';
-import { MediaPhone, MediaTablet } from './utility/media';
+import { MediaPhone, MediaTablet, MediaDesktop } from './utility/media';
 import { PhoneMenu } from './Nav/PhoneNav';
+import { TabletMenu } from './Nav/TabletNav';
+import { ComputerMenu } from './Nav/ComputerNav';
 
 export function Navbar() {
   return (
     <Menu className="navbar">
       <Title position="navbar" />
       <Consumer>
-        {({ user }) => (user ? (
+        {({ user, history }) => (
           <>
             <MediaPhone>
-              <PhoneMenu user={user} />
+              <PhoneMenu user={user} history={history} />
             </MediaPhone>
             <MediaTablet>
-            I'm a tablet
+              <TabletMenu user={user} history={history} />
             </MediaTablet>
+            <MediaDesktop>
+              <ComputerMenu user={user} history={history} />
+            </MediaDesktop>
           </>
-        ) : (
-          <Button icon>
-            <Icon name="sign-in" />
-          </Button>
-        ))}
+        )}
       </Consumer>
 
 
