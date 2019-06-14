77c9546ce09f3e2276495bcfcb52aedfff3daad4
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-06-13 21:33:15 -0500

    Commented console logs for image compression function and added return error if there is a failure.

--- src/components/utility/compressImage.js
+++ src/components/utility/compressImage.js
@@ -20,13 +20,17 @@ const options = {
 
 export async function compressImage(imageFile, imageType) {
   // imageType should be "mainImg", "thumbImg", or "avatarImg"
-  console.log('original file instanceof Blob', imageFile instanceof Blob);
-  console.log(`original file size:  ${imageFile.size / 1024 / 1024} MB`);
+  // console.log('original file instanceof Blob', imageFile instanceof Blob);
+  // console.log(`original file size:  ${imageFile.size / 1024 / 1024} MB`);
 
   try {
     const compressedFile = await shrinkImage(imageFile, options[imageType]);
-    console.log('compressed file instanceof Blob', compressedFile instanceof Blob);
-    console.log(`compressed file size:  ${compressedFile.size / 1024 / 1024} MB`);
+    // console.log('compressed file instanceof Blob', compressedFile instanceof Blob);
+    // console.log(`compressed file size:  ${compressedFile.size / 1024 / 1024} MB`);
     return compressedFile;
-  } catch (e) { console.log(e); }
+  } catch (e) {
+    /* eslint-disable-next-line */
+    console.log(e);
+    return e;
+  }
 }
