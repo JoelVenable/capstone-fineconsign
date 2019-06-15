4782f4c106570f6741ebca9df80a77fc87e5fdd4
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-06-15 09:48:35 -0500

    Added error handling for painting not found.

--- src/components/PaintingList/EditPainting.jsx
+++ src/components/PaintingList/EditPainting.jsx
@@ -1,15 +1,37 @@
 import React, { useState } from 'react';
 import {
-  Input, TextArea, Form, Button, Message,
+  Input, TextArea, Form, Button, Message, Container,
 } from 'semantic-ui-react';
 import './addPainting.css';
 import PropTypes from 'prop-types';
 import { compressImage } from '../utility/compressImage';
 
 export function EditPainting({
-  user, id, storageRef, artists, edit, history, paintings,
+  user, id, storageRef, artists, edit, history, paintings, showError,
 }) {
-  const painting = paintings.find(item => item.id === id);
+  console.log('id', id);
+  console.log('paintings', paintings);
+
+  const [error, setError] = useState({
+    isVisible: true,
+    header: 'Painting not found!',
+    content: <Button>Go back...</Button>,
+  });
+
+
+  let painting = paintings.find(item => item.id === id);
+  //  Set Error condition if painting not found
+  if (painting === undefined) {
+    painting = {
+      artistId: NaN,
+      name: '',
+      description: '',
+      originalPrice: NaN,
+      medium: '',
+      height: NaN,
+      width: NaN,
+    };
+  }
 
   const [artistId, setArtistId] = useState(painting.artistId);
   const [name, setName] = useState(painting.name);
@@ -20,12 +42,6 @@ export function EditPainting({
   const [medium, setMedium] = useState(painting.medium);
   const [height, setHeight] = useState(painting.height);
   const [width, setWidth] = useState(painting.width);
-
-  const [error, setError] = useState({
-    isVisible: false,
-    header: '',
-    content: '',
-  });
 
 
   async function handleSubmit(e, firebaseStorage) {
@@ -58,105 +74,107 @@ export function EditPainting({
 
 
   return (
-    <Form error={error.isVisble} loading={loading} onSubmit={e => handleSubmit(e, storageRef)}>
-      <Form.Field
-        required
-        value={name}
-        onChange={(_e, { value }) => setName(value)}
-        control={Input}
-        label="Painting Name"
-        placeholder="Painting name"
-        id="name"
-      />
-
-      <Form.Group widths={8}>
-        <Form.Select
-          width="6"
-          label="Artist Name"
-          required
-          disabled={user.userType === 'artist'}
-          options={artists.map(artist => ({
-            key: artist.id,
-            text: `${artist.firstName} ${artist.lastName}`,
-            value: artist.id,
-            image: { avatar: true, src: artist.artistImageUrl },
-          }))}
-          onChange={(_e, { value }) => setArtistId(value)}
-          value={artistId}
-          id="artistId"
-        />
-
+    <Container>
+      <Form error={error.isVisble} loading={loading} onSubmit={e => handleSubmit(e, storageRef)}>
         <Form.Field
-          width="6"
-
           required
-          value={medium}
-          onChange={(_e, { value }) => setMedium(value)}
+          value={name}
+          onChange={(_e, { value }) => setName(value)}
           control={Input}
-          label="Medium"
-          placeholder="Oil on Canvas"
+          label="Painting Name"
+          placeholder="Painting name"
+          id="name"
         />
-        <Form.Field
+
+        <Form.Group widths={8}>
+          <Form.Select
+            width="6"
+            label="Artist Name"
+            required
+            disabled={user.userType === 'artist'}
+            options={artists.map(artist => ({
+              key: artist.id,
+              text: `${artist.firstName} ${artist.lastName}`,
+              value: artist.id,
+              image: { avatar: true, src: artist.artistImageUrl },
+            }))}
+            onChange={(_e, { value }) => setArtistId(value)}
+            value={artistId}
+            id="artistId"
+          />
+
+          <Form.Field
+            width="6"
+
+            required
+            value={medium}
+            onChange={(_e, { value }) => setMedium(value)}
+            control={Input}
+            label="Medium"
+            placeholder="Oil on Canvas"
+          />
+          <Form.Field
+            required
+            control="input"
+            type="number"
+            label="Price"
+            onChange={e => setPrice(e.target.value)}
+            placeholder="Suggest a price for us"
+            width="6"
+          />
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Field
+            control="input"
+            width="4"
+            required
+
+            type="number"
+            label="Height in inches"
+            onChange={e => setHeight(e.target.value)}
+            placeholder="24"
+          />
+          <Form.Field
+            width="4"
+            required
+
+            control="input"
+            type="number"
+            label="Width in inches"
+            onChange={e => setWidth(e.target.value)}
+            placeholder="36"
+          />
+          <Form.Field
+            width="8"
+            required
+
+            control="input"
+            type="file"
+            label="Photo"
+            onChange={e => setPhoto(e.target.files[0])}
+            placeholder="Upload an image"
+          />
+        </Form.Group>
+
+        <TextArea
+          value={description}
+          onChange={(_e, { value }) => setDescription(value)}
           required
-          control="input"
-          type="number"
-          label="Price"
-          onChange={e => setPrice(e.target.value)}
-          placeholder="Suggest a price for us"
-          width="6"
+
+          rows={7}
+          placeholder="Painting description"
         />
-      </Form.Group>
 
-      <Form.Group>
-        <Form.Field
-          control="input"
-          width="4"
-          required
 
-          type="number"
-          label="Height in inches"
-          onChange={e => setHeight(e.target.value)}
-          placeholder="24"
+        <Button type="submit" content="Save" color="purple" width="4" />
+        <Message
+          error
+          header={error.header}
+          content={error.content}
         />
-        <Form.Field
-          width="4"
-          required
-
-          control="input"
-          type="number"
-          label="Width in inches"
-          onChange={e => setWidth(e.target.value)}
-          placeholder="36"
-        />
-        <Form.Field
-          width="8"
-          required
-
-          control="input"
-          type="file"
-          label="Photo"
-          onChange={e => setPhoto(e.target.files[0])}
-          placeholder="Upload an image"
-        />
-      </Form.Group>
-
-      <TextArea
-        value={description}
-        onChange={(_e, { value }) => setDescription(value)}
-        required
-
-        rows={7}
-        placeholder="Painting description"
-      />
-
-
-      <Button type="submit" content="Save" color="purple" width="4" />
-      <Message
-        error
-        header={error.header}
-        content={error.content}
-      />
-    </Form>
+      </Form>
+    </Container>
   );
 }
 
@@ -181,5 +199,5 @@ EditPainting.propTypes = {
   history: PropTypes.shape({
     push: PropTypes.func.isRequired,
   }).isRequired,
-
+  showError: PropTypes.func.isRequired,
 };
