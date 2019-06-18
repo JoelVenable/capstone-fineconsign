c1dec7a5a71596d5f39e0f4df048443801e74a17
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-06-18 13:09:40 -0500

    Wrapped app with user context

--- src/index.jsx
+++ src/index.jsx
@@ -5,15 +5,16 @@ import 'semantic-ui-css/semantic.min.css';
 import { BrowserRouter } from 'react-router-dom';
 import { ContextProvider } from './ContextProvider';
 import { ApplicationViews } from './ApplicationViews';
+import { UserProvider } from './AllUsersContext';
 
 
 ReactDOM.render(
   <BrowserRouter>
     <ContextProvider>
-
-      <ApplicationViews />
+      <UserProvider>
+        <ApplicationViews />
+      </UserProvider>
     </ContextProvider>
-
   </BrowserRouter>,
   document.getElementById('root'),
 );
