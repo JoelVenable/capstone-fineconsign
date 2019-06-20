af734ae2339418ab830597c82e81573db331dcd9
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-06-20 09:01:30 -0500

    Built out Cart and copied paintingTableItem

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,12 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Header, Table, Grid } from 'semantic-ui-react';
+import { PaintingCartItem } from './PaintingCartItem';
+
 
 export function Cart({
-  get,
+  get, myCart, history, user, artists,
 }) {
+  console.log('artists', artists);
   return (
     <>
+      <Header as="h1">
+    My Cart
+      </Header>
+      <Grid colums="two" divided>
+        <Table celled>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Painting Name</Table.HeaderCell>
+              <Table.HeaderCell>Artist</Table.HeaderCell>
+              <Table.HeaderCell>Price</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          {myCart.orderItems.map(item => (
+            <PaintingCartItem
+              painting={item}
+              history={history}
+              artist={artists.find((artist) => {
+                console.log('artist', artist);
+                console.log('item', item);
 
+                return artist.id === item.artistId;
+              })}
+              user={user}
+              key={item.id}
+            />
+          )) }
+        </Table>
+      </Grid>
     </>
   );
 }
---
+++ src/components/Cart/PaintingCartItem.jsx
@@ -0,0 +1,194 @@
+import React from 'react';
+import {
+  Image, Button, Icon, Table, Header,
+} from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { Consumer } from '../../ContextProvider';
+
+export function PaintingCartItem({
+  painting, user: { userType }, history, artist,
+}) {
+  console.log('artist from PaintingCartItem', artist);
+  console.log('painting from PainitngCartItem', painting);
+  return (
+    <Table.Row>
+
+      <Table.Cell selectable>
+        <Link to={`/gallery/${painting.id}`}>
+          <Header
+            as="h4"
+            image
+            style={{
+              minHeight: '58px', paddingLeft: '.5rem', display: 'flex', flexDirection: 'row', alignItems: 'center',
+            }}
+          >
+            <Image src={painting.thumbUrl} />
+            <Header.Content>
+              <strong>{painting.name}</strong>
+              {(userType === 'artist') ? null : (
+                <Header.Subheader>
+                  {/* {`${artist.firstName} ${artist.lastName}`} */}
+                </Header.Subheader>
+              )}
+            </Header.Content>
+          </Header>
+        </Link>
+      </Table.Cell>
+      <Table.Cell>
+        {showStatus(userType, painting)}
+      </Table.Cell>
+      <Table.Cell>
+        {showControls(userType, painting, history)}
+
+      </Table.Cell>
+
+    </Table.Row>
+  );
+}
+
+function showControls(userType, {
+  isSubmitted, isLive, isSold, id,
+}, history) {
+  if (userType === 'artist') {
+    if (isSold) return null;
+    if (isLive) return null;
+    if (isSubmitted) return null;
+    return (
+      <div className="table-actionIconContainer">
+        <EditButton id={id} history={history} />
+
+        <Consumer>
+          {({ edit, showConfirm }) => (
+            <Button
+              icon
+              color="green"
+              onClick={() =>
+                showConfirm({
+                  title: 'Send Painting for Employee Review', // REQUIRED.  The title of the message requesting delete confirmation
+                  text: 'Please confirm; you cannot reverse this action.', // The inner content of text to be displayed
+                  confirmAction: () => edit.painting({ isSubmitted: true }, id), // Function called when action is confirmed
+                  confirmBtnColor: 'green', // String value.  Accepts color of confirmation button.
+                  icon: 'arrow circle right', // String value or null.  Icon next to the title
+                  btnIcon: 'send', // String value or null.  Icon inside the confirmation button
+                  btnText: 'Send it!',
+                })}
+            >
+              <Icon name="send" />
+            </Button>
+          )}
+        </Consumer>
+      </div>
+    );
+  }
+  if (userType === 'employee') {
+    if (isSold) return null;
+    if (isLive) {
+      return (
+        <div className="table-actionIconContainer">
+          <EditButton id={id} history={history} />
+
+        </div>
+      );
+    }
+    if (isSubmitted) {
+      return (
+        <div className="table-actionIconContainer">
+          <EditButton id={id} history={history} />
+          <Consumer>
+            {({ edit, showConfirm }) => (
+              <Button
+                icon
+                color="green"
+                onClick={() =>
+                  showConfirm({
+                    title: 'Show this painting to customers!', // REQUIRED.  The title of the message requesting delete confirmation
+                    text: '', // The inner content of text to be displayed
+                    confirmAction: () => edit.painting({ isLive: true }, id), // Function called when action is confirmed
+                    confirmBtnColor: 'green', // String value.  Accepts color of confirmation button.
+                    icon: 'bullhorn', // String value or null.  Icon next to the title
+                    btnIcon: 'fire', // String value or null.  Icon inside the confirmation button
+                    btnText: 'Go Live!',
+                  })}
+              >
+                <Icon name="send" />
+              </Button>
+            )}
+          </Consumer>
+        </div>
+      );
+    }
+  }
+  //  Under normal circumstances, this final return
+  //  statement should never be reached...
+  return null;
+}
+
+function showStatus(userType, {
+  isSubmitted, isLive, isSold, currentPrice, isReviewed,
+}) {
+  if (userType === 'artist') {
+    if (isSold) return <Header as="h4" color="green">{`Sold for $${currentPrice}`}</Header>;
+    if (isLive) return <Header as="h4" color="blue">For Sale</Header>;
+    if (isSubmitted) {
+      if (isReviewed) return <Header as="h4" color="violet">Reviewed</Header>;
+      return <Header as="h4" color="olive">Submitted</Header>;
+    }
+    return <Header as="h4" color="orange">Draft</Header>;
+  }
+
+  if (userType === 'employee') {
+    if (isSold) return <Header as="h4" color="green">{`Sold for $${currentPrice}`}</Header>;
+    if (isLive) return <Header as="h4" color="blue">For Sale</Header>;
+    if (isSubmitted) {
+      if (isReviewed) return <Header as="h4" color="violet">Reviewed</Header>;
+      return <Header as="h4" color="orange">Needs Review</Header>;
+    }
+  }
+
+  // this line should never be reached...
+  return null;
+}
+
+PaintingCartItem.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+  painting: PropTypes.shape({
+    artist: PropTypes.shape({
+      firstName: PropTypes.string.isRequired,
+      lastName: PropTypes.string.isRequired,
+    }).isRequired,
+    //   artistId: PropTypes.number.isRequired,
+    // currentPrice: PropTypes.number.isRequired,
+    // forSaleDate: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+    // imgUrl: PropTypes.string.isRequired,
+    // liveDescription: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    originalPrice: PropTypes.number.isRequired,
+    priceAdjustmentId: PropTypes.number.isRequired,
+    submittedDescription: PropTypes.string.isRequired,
+    thumbUrl: PropTypes.string.isRequired,
+  }).isRequired,
+  user: PropTypes.shape({
+    userType: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+
+function EditButton({ id, history }) {
+  return (
+    <Button icon>
+      <Icon name="edit" onClick={() => history.push(`/paintings/${id}/edit`)} />
+    </Button>
+  );
+}
+
+
+EditButton.propTypes = {
+  id: PropTypes.number.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
