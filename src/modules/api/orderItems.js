ad2bd2fe2977f1547ab8ed30605001f6db4b1d16
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-06-20 08:37:38 -0500

    Added new functions to API

--- src/modules/api/orderItems.js
+++ src/modules/api/orderItems.js
@@ -6,6 +6,8 @@ const endpoint = new Endpoint(`${baseURL}/orderItems`);
 
 export const orderItems = {
   create: obj => endpoint.create(obj),
+  findExisting: (orderId, paintingId) => (
+    endpoint.getAll(`?orderId=${orderId}&paintingId=${paintingId}`)),
   getAll: () => endpoint.getAll(),
   edit: (id, newParams) => endpoint.update(id, newParams),
   delete: id => endpoint.delete(id),
--- src/modules/api/orders.js
+++ src/modules/api/orders.js
@@ -7,6 +7,18 @@ const endpoint = new Endpoint(`${baseURL}/orders`);
 export const orders = {
   create: obj => endpoint.create(obj),
   getAll: () => endpoint.getAll(),
+  getMine: customerId => endpoint.getAll(`?customerId=${customerId}`),
+  getMyOpenCart: customerId => endpoint.getAll(`?customerId=${customerId}&isSubmitted=false&_embed=orderItems`)
+    .then((results) => {
+      console.log('getOpenCart results:', results);
+      /* eslint-disable-next-line */
+      if (results.length > 1) {
+        console.log('Warning, multiple carts found!');
+      }
+
+      if (results.length > 0) return results;
+      return ['not found'];
+    }),
   edit: (id, newParams) => endpoint.update(id, newParams),
   delete: id => endpoint.delete(id),
 };
