99ddd21c73ac6d32f798773fb1a32efc2374f717
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-06-21 10:51:19 -0500

    Removed login/register reference from welcome

--- src/components/welcome/Welcome.jsx
+++ src/components/welcome/Welcome.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { HeroImage } from './HeroImage';
 import { Title } from './Title';
-import { SelectLoginRegister } from './SelectLoginRegister';
 import './welcome.css';
 
 export function Welcome() {
