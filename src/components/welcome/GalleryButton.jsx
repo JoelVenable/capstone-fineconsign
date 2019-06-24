c83037ad873f7ebf083ae97ee03d47277167b7a1
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-06-24 08:33:12 -0400

    Added 'go to gallery' button to home screen

--- src/components/welcome/GalleryButton.jsx
+++ src/components/welcome/GalleryButton.jsx
@@ -1,23 +1,41 @@
 import React from 'react';
-import { Button, Icon } from 'semantic-ui-react';
+import {
+  Button, Icon, Responsive, Image, Header,
+} from 'semantic-ui-react';
 import { Consumer } from '../../ContextProvider';
-
+import largeImg from './assets/gallery-lg.jpg';
+import smallImg from './assets/gallery-sm.jpg';
 
 export function GalleryButton() {
   return (
 
     <Consumer>
-      {({ history }) => (
-        <Button
-          fluid
-          icon
-          labelPosition="left"
-          primary
-          onClick={() => history.push('/gallery')}
-        >
-          <Icon name="images outline" />
-Go to Gallery
-        </Button>
+      {({ history, showLogin }) => (
+        <div className="loginRegisterBox">
+          <Header className="loginRegisterBox--header">Go to Gallery</Header>
+          <Image
+            src={largeImg}
+            onClick={() => history.push('/gallery')}
+            fluid
+            style={{
+              cursor: 'pointer',
+              borderRadius: '6px',
+              marginBottom: '2rem',
+            }}
+          />
+
+          <Button
+            fluid
+            icon
+            labelPosition="left"
+            onClick={showLogin}
+          >
+            <Icon name="sign-in" />
+Log in
+          </Button>
+
+        </div>
+
       )
   }
     </Consumer>
