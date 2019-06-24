411a741b695f6d8ed118cab17f0b21bb4e6e281c
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-06-24 08:35:00 -0400

    Referenced new component on welcome screen.

--- src/components/welcome/Welcome.jsx
+++ src/components/welcome/Welcome.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { HeroImage } from './HeroImage';
 import { Title } from './Title';
 import './welcome.css';
+import { GalleryButton } from './GalleryButton';
 
 export function Welcome() {
   return (
     <div>
       <HeroImage />
       <Title position="hero" />
+      <GalleryButton />
     </div>
   );
 }
