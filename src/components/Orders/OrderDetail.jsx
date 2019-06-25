f7281b8488e559f301f0d44db89ac03ba7576c0f
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-06-25 12:21:16 -0400

    Updated Order detail view

--- src/components/Orders/OrderDetail.jsx
+++ src/components/Orders/OrderDetail.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import {
-  Header, Table, Transition, Button, Icon, Loader, Segment, Dimmer, Card, Responsive,
+  Header, Table, Transition, Button, Icon, Loader, Segment, Dimmer, Card, Responsive, Item, Grid,
 } from 'semantic-ui-react';
 import { PaintingOrderItem } from './PaintingOrderItem';
 import { Consumer } from '../../ContextProvider';
@@ -11,6 +11,7 @@ export function OrderDetail({ id }) {
   console.log(id);
   const [loading, setLoading] = useState(false);
   const [success, setSuccess] = useState(false);
+  const [authorized, setAuthorized] = useState(false);
 
 
   return (
@@ -18,10 +19,14 @@ export function OrderDetail({ id }) {
       {({
         orders, paintings, history, removeFromCart, user, edit,
       }) => {
-        console.log('orders', orders);
         const order = orders.find(item => item.id === id);
-        console.log('found order', order);
+        const customer = order ? order.customer : null;
+        console.log('customer', customer);
         const orderedPaintings = order ? order.orderItems.map(orderItem => paintings.find(item => item.id === orderItem.paintingId)) : null;
+
+        //  Check if user is authorized to view this order
+
+
         // The 'isDefined' variable checks to see if data has been loaded.
         // Otherwise the page will break as it will try to access properties of an undefined object
         // (happens on initial paint before the fetch call resolves)
@@ -30,72 +35,97 @@ export function OrderDetail({ id }) {
           <>
             {isDefined ? (
               <>
+                <Segment.Group>
+                  <Segment>
+                    <Header as="h1" style={{ marginBottom: '2rem' }}>
+                      {`Order number: ${id}`}
+                    </Header>
 
-                <Header as="h1" style={{ marginBottom: '2rem' }}>
-Order number:
-                  {' '}
-                  {id}
-                </Header>
-                <Segment>
-                  <Dimmer active={loading}>
-                    <Loader>
+                    <Dimmer active={loading}>
+                      <Loader>
 Submitting...
-                    </Loader>
-                  </Dimmer>
+                      </Loader>
+                    </Dimmer>
 
-                  <Dimmer active={success}>
-                    <Card>
+                    <Dimmer active={success}>
+                      <Card>
 
-                      <Card.Content>
-                        <Card.Header>Your order has been processed!</Card.Header>
-                        <Card.Description>
-  We will be in touch soon.
-                        </Card.Description>
+                        <Card.Content>
+                          <Card.Header>This order has been processed!</Card.Header>
 
+                        </Card.Content>
 
-                      </Card.Content>
+                      </Card>
 
-                    </Card>
+                    </Dimmer>
+                  </Segment>
+                  <Segment>
+                    {customer ? (
+                      <Grid columns={2}>
+                        <Grid.Row>
+                          <Grid.Column>
+                            <Header as="h3">Customer Data:</Header>
+                            <p style={{ marginTop: '2rem' }}>
+                              {`Account Balance: $${customer.accountBalance}`}
+                            </p>
+                          </Grid.Column>
+                          <Grid.Column>
+                            <Item>
+                              <Item.Header as="h5">
+                                {`${customer.firstName} ${customer.lastName}`}
+                              </Item.Header>
+                              <Item.Meta>
+                                {customer.address}
+                              </Item.Meta>
+                              <Item.Meta>
+                                {`${customer.city}, ${customer.state} ${customer.zipcode}`}
+                              </Item.Meta>
 
-                  </Dimmer>
+                            </Item>
+                          </Grid.Column>
+                        </Grid.Row>
+                      </Grid>
+                    ) : null}
+                  </Segment>
+                  <Segment>
+                    <Table celled>
 
+                      <Table.Header>
+                        <Table.Row>
 
-                  <Table celled>
+                          <Table.HeaderCell>
+                            <Responsive minWidth={767}>Painting Name</Responsive>
+                            <Responsive maxWidth={766}>Items:</Responsive>
+                          </Table.HeaderCell>
+                          <Table.HeaderCell>
+                            <Responsive minWidth={767}>Artist</Responsive>
+                          </Table.HeaderCell>
+                          <Table.HeaderCell>
+                            <Responsive minWidth={767}>Price</Responsive>
+                          </Table.HeaderCell>
+                        </Table.Row>
+                      </Table.Header>
 
-                    <Table.Header>
-                      <Table.Row>
+                      <Table.Body>
+                        <Transition.Group animation="slide down" duration={300}>
+                          {isDefined ? orderedPaintings.map(painting => (
+                            <PaintingOrderItem
+                              painting={painting}
+                              history={history}
+                              removeFromCart={removeFromCart}
+                              user={user}
+                              key={painting.id}
+                            />
+                          )) : null}
 
-                        <Table.HeaderCell>
-                          <Responsive minWidth={767}>Painting Name</Responsive>
-                          <Responsive maxWidth={766}>Items:</Responsive>
-                        </Table.HeaderCell>
-                        <Table.HeaderCell>
-                          <Responsive minWidth={767}>Artist</Responsive>
-                        </Table.HeaderCell>
-                        <Table.HeaderCell>
-                          <Responsive minWidth={767}>Price</Responsive>
-                        </Table.HeaderCell>
-                      </Table.Row>
-                    </Table.Header>
-
-                    <Table.Body>
-                      <Transition.Group animation="slide down" duration={300}>
-                        {isDefined ? orderedPaintings.map(painting => (
-                          <PaintingOrderItem
-                            painting={painting}
-                            history={history}
-                            removeFromCart={removeFromCart}
-                            user={user}
-                            key={painting.id}
-                          />
-                        )) : null}
-
-                      </Transition.Group>
-                    </Table.Body>
-                  </Table>
-
-
-                </Segment>
+                        </Transition.Group>
+                      </Table.Body>
+                    </Table>
+                  </Segment>
+                  <Segment>
+                    <Header as="h3">Order Status: </Header>
+                  </Segment>
+                </Segment.Group>
 
               </>
             ) : null}
