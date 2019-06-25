3944933390e1561bb0d56c787cfdf86655ab97a0
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-06-25 12:20:41 -0400

    Fixed error if not logged in

--- src/components/PaintingList/PaintingControls.jsx
+++ src/components/PaintingList/PaintingControls.jsx
@@ -18,52 +18,55 @@ export function PaintingControls({ id }) {
         const {
           isSold, isLive, isSubmitted, isPendingSale,
         } = paintings.find(item => item.id === id);
-        if (user.userType === 'artist') {
-          if (isSold || isLive || isSubmitted || isPendingSale) return null;
-          return (
-            <div className="table-actionIconContainer">
-              <EditButton id={id} history={history} />
-              <SendForReviewButton id={id} />
-            </div>
-          );
-        }
-        if (user.userType === 'employee') {
-          if (isSold || isPendingSale) {
+        if (user) {
+          if (user.userType === 'artist') {
+            if (isSold || isLive || isSubmitted || isPendingSale) return null;
+            return (
+              <div className="table-actionIconContainer">
+                <EditButton id={id} history={history} />
+                <SendForReviewButton id={id} />
+              </div>
+            );
+          }
+
+          if (user.userType === 'employee') {
+            if (isSold || isPendingSale) {
             //  find all order items containing the painting
-            const orderId = orderItems.filter(item => item.paintingId === id)
+              const orderId = orderItems.filter(item => item.paintingId === id)
 
-            //  then make an orders array from values referenced in the orderItems table
-              .map(item => orders.find(order => order.id === item.orderId))
+              //  then make an orders array from values referenced in the orderItems table
+                .map(item => orders.find(order => order.id === item.orderId))
 
-            //  then find the order that triggered the current 'isPendingSale' flag.
-              .find(item => item.isSubmitted && !item.isRejected)
-              .id;
+              //  then find the order that triggered the current 'isPendingSale' flag.
+                .find(item => item.isSubmitted && !item.isRejected)
+                .id;
 
-            return (
-              <div className="table-actionIconContainer">
+              return (
+                <div className="table-actionIconContainer">
 
-                <OrderButton id={orderId} history={history} />
+                  <OrderButton id={orderId} history={history} />
 
 
-              </div>
-            );
-          }
-          if (isLive) {
-            return (
-              <div className="table-actionIconContainer">
-                <DeactivateButton id={id} />
-                <EditButton id={id} history={history} />
-              </div>
-            );
-          }
-          if (isSubmitted) {
-            return (
-              <div className="table-actionIconContainer">
-                <KickbackButton id={id} />
-                <EditButton id={id} history={history} />
-                <GoLiveButton id={id} />
-              </div>
-            );
+                </div>
+              );
+            }
+            if (isLive) {
+              return (
+                <div className="table-actionIconContainer">
+                  <DeactivateButton id={id} />
+                  <EditButton id={id} history={history} />
+                </div>
+              );
+            }
+            if (isSubmitted) {
+              return (
+                <div className="table-actionIconContainer">
+                  <KickbackButton id={id} />
+                  <EditButton id={id} history={history} />
+                  <GoLiveButton id={id} />
+                </div>
+              );
+            }
           }
         }
         return null;
