d3d243ae5109917e41f2b43c02b3bdef2c7f7a97
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-06-28 09:42:50 -0400

    Order detail now shows cancelled or processed status

--- src/components/Orders/OrderDetail.jsx
+++ src/components/Orders/OrderDetail.jsx
@@ -25,6 +25,7 @@ export function OrderDetail({ id }) {
       }) => {
         const order = orders.find(item => item.id === id);
         const customer = order ? order.customer : null;
+        const showControls = order ? !order.isCancelled && !order.isApproved : false;
         const orderedPaintings = order ? (
           order.orderItems.map((orderItem) => {
             const painting = paintings.find(item => item.id === orderItem.paintingId);
@@ -41,149 +42,163 @@ export function OrderDetail({ id }) {
         // (happens on initial paint before the fetch call resolves)
         const isDefined = order ? !!orderedPaintings[0] : false;
 
-        return (
-          <>
-            {isDefined ? (
-              <>
-                <Segment.Group>
-                  <CancelOrderModal
-                    isModalVisible={isModalVisible}
-                    handleClose={handleClose}
-                    edit={edit}
-                    updateAll={updateAll}
-                    orderId={id}
-                  />
-                  <Segment>
-                    <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
-                      <Header as="h1" style={{ marginBottom: '2rem' }}>
-                        {`Order number: ${id}`}
-                      </Header>
-                      {(order.isCancelled || order.isApproved) ? null : (
-                        <div>
-                          <Button negative style={{ marginRight: '1rem' }} onClick={() => setIsModalVisible(true)}>
-                          Cancel Order
-                          </Button>
-                          <Button primary>
-Approve Order
-                          </Button>
+        return isDefined ? (
+          <Segment.Group>
+            <CancelOrderModal
+              isModalVisible={isModalVisible}
+              handleClose={handleClose}
+              edit={edit}
+              updateAll={updateAll}
+              orderId={id}
+            />
+            <Dimmer active={loading}>
+              <Loader content="Submitting..." />
+            </Dimmer>
+            <Dimmer active={success}>
+              <Card content={
+                <Header content="This order has been processed!" />}
+              />
+            </Dimmer>
+            <Segment
+              disabled={loading}
+            >
+
+              <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+                <Header
+                  as="h1"
+                  style={{ marginBottom: '2rem' }}
+                  content={`Order number: ${id}`}
+                />
+                {showControls ? (
+                  <div>
+                    <Button
+                      negative
+                      style={{ marginRight: '1rem' }}
+                      onClick={() => setIsModalVisible(true)}
+                      content="Cancel Order"
+                    />
+                    <Button
+                      content="Approve Order"
+                      primary
+                      onClick={() => {
+                        setLoading(true);
+                        setTimeout(() => {
+                          setSuccess(true);
+                          setLoading(false);
+                        });
+                      }}
+                    />
 
 
-                        </div>
-                      )}
-
-                    </div>
-                    <Dimmer active={loading}>
-                      <Loader>
-Submitting...
-                      </Loader>
-                    </Dimmer>
-
-                    <Dimmer active={success}>
-                      <Card>
-
-                        <Card.Content>
-                          <Card.Header>This order has been processed!</Card.Header>
-
-                        </Card.Content>
-
-                      </Card>
-
-                    </Dimmer>
-                  </Segment>
-                  <Segment>
-                    {customer ? (
-                      <Grid columns={2}>
-                        <Grid.Row>
-                          <Grid.Column>
-                            <Header as="h3">Customer Data:</Header>
-                            <p style={{ marginTop: '2rem' }}>
-                              {`Account Balance: $${customer.accountBalance}`}
-                            </p>
-                          </Grid.Column>
-                          <Grid.Column>
-                            <Item>
-                              <Item.Header as="h5">
-                                {`${customer.firstName} ${customer.lastName}`}
-                              </Item.Header>
-                              <Item.Meta>
-                                {customer.address}
-                              </Item.Meta>
-                              <Item.Meta>
-                                {`${customer.city}, ${customer.state} ${customer.zipcode}`}
-                              </Item.Meta>
-
-                            </Item>
-                          </Grid.Column>
-                        </Grid.Row>
-                      </Grid>
+                  </div>
+                ) : (
+                  <div>
+                    {order.isCancelled ? (
+                      <Header as="h3" color="red" content="Order Cancelled" />
                     ) : null}
-                  </Segment>
-                  <Segment>
-                    <Table celled>
+                    {order.isApproved ? (
+                      <Header as="h3" color="blue" content="Order Completed" />
+                    ) : null}
 
-                      <Table.Header>
-                        <Table.Row>
+                  </div>
+                )}
 
-                          <Table.HeaderCell>
-                            <Responsive minWidth={767}>Painting Name</Responsive>
-                            <Responsive maxWidth={766}>Items:</Responsive>
-                          </Table.HeaderCell>
-                          <Table.HeaderCell>
-                            <Responsive minWidth={767}>Artist</Responsive>
-                          </Table.HeaderCell>
-                          <Table.HeaderCell>
-                            <Responsive minWidth={767}>Price</Responsive>
-                          </Table.HeaderCell>
-                        </Table.Row>
-                      </Table.Header>
+              </div>
 
-                      <Table.Body>
-                        <Transition.Group animation="slide down" duration={300}>
-                          {isDefined ? orderedPaintings.map(painting => (
-                            <PaintingOrderItem
-                              painting={painting}
-                              history={history}
-                              edit={edit}
-                              updateAll={updateAll}
-                              user={user}
-                              key={painting.id}
-                            />
-                          )) : null}
 
-                        </Transition.Group>
-                      </Table.Body>
-                      <Table.Footer>
-                        <Table.Row>
-                          <Table.HeaderCell colSpan="3">
-                            <div style={{
-                              display: 'flex',
-                              flexDirection: 'row',
-                              justifyContent: 'space-between',
-                              fontWeight: 'bold',
-                            }}
-                            >
-                              <span>Order Total:</span>
-                              <span>
+            </Segment>
+            <Segment
+              disabled={loading || order.isCancelled}
+            >
+              {customer ? (
+                <Grid columns={2}>
+                  <Grid.Row>
+                    <Grid.Column>
+                      <Header as="h3">Customer Data:</Header>
+                      <p style={{ marginTop: '2rem' }}>
+                        {`Account Balance: $${customer.accountBalance}`}
+                      </p>
+                    </Grid.Column>
+                    <Grid.Column>
+                      <Item>
+                        <Item.Header as="h5">
+                          {`${customer.firstName} ${customer.lastName}`}
+                        </Item.Header>
+                        <Item.Meta>
+                          {customer.address}
+                        </Item.Meta>
+                        <Item.Meta>
+                          {`${customer.city}, ${customer.state} ${customer.zipcode}`}
+                        </Item.Meta>
+
+                      </Item>
+                    </Grid.Column>
+                  </Grid.Row>
+                </Grid>
+              ) : null}
+            </Segment>
+            <Segment
+              disabled={loading || order.isCancelled}
+            >
+              <Table celled>
+
+                <Table.Header>
+                  <Table.Row>
+
+                    <Table.HeaderCell>
+                      <Responsive minWidth={767}>Painting Name</Responsive>
+                      <Responsive maxWidth={766}>Items:</Responsive>
+                    </Table.HeaderCell>
+                    <Table.HeaderCell>
+                      <Responsive minWidth={767}>Artist</Responsive>
+                    </Table.HeaderCell>
+                    <Table.HeaderCell>
+                      <Responsive minWidth={767}>Price</Responsive>
+                    </Table.HeaderCell>
+                  </Table.Row>
+                </Table.Header>
+
+                <Table.Body>
+                  <Transition.Group animation="slide down" duration={300}>
+                    {isDefined ? orderedPaintings.map(painting => (
+                      <PaintingOrderItem
+                        painting={painting}
+                        history={history}
+                        edit={edit}
+                        showControls={showControls}
+                        updateAll={updateAll}
+                        user={user}
+                        key={painting.id}
+                      />
+                    )) : null}
+
+                  </Transition.Group>
+                </Table.Body>
+                <Table.Footer>
+                  <Table.Row>
+                    <Table.HeaderCell colSpan="3">
+                      <div style={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        justifyContent: 'space-between',
+                        fontWeight: 'bold',
+                      }}
+                      >
+                        <span>Order Total:</span>
+                        <span>
 $
-                                <CalculateOrderTotal orderId={id} />
-                              </span>
-                            </div>
+                          <CalculateOrderTotal orderId={id} />
+                        </span>
+                      </div>
 
-                          </Table.HeaderCell>
-                        </Table.Row>
+                    </Table.HeaderCell>
+                  </Table.Row>
 
-                      </Table.Footer>
-                    </Table>
-                  </Segment>
-                  <Segment>
-                    <Header as="h3">Order Status: </Header>
-                  </Segment>
-                </Segment.Group>
-
-              </>
-            ) : null}
-          </>
-        );
+                </Table.Footer>
+              </Table>
+            </Segment>
+          </Segment.Group>
+        ) : null;
       }}
     </Consumer>
   );
