f04c980d799117efb7816956599574b4569a147f
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-06-30 11:09:51 -0500

    Refactored delete painting function.
    It now removes the artistId reference prior to deleting.
    This fixes the 500 error in JSON-server.

--- src/components/utility/DeleteButton.jsx
+++ src/components/utility/DeleteButton.jsx
@@ -1,29 +1,43 @@
 import React from 'react';
-import { Button } from 'semantic-ui-react';
+import { Button, Icon, Popup } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { Consumer } from '../../ContextProvider';
 
-export function DeleteButton({ id, type }) {
+export function DeleteButton({ id, type, showText }) {
   return (
     <Consumer>
-      {({ showConfirm, remove }) => (
-        <Button
-          color="red"
-          width="4"
-          onClick={(e) => {
-            e.preventDefault();
-            showConfirm({
-              title: `Are you sure you want to delete this ${type}?`, // REQUIRED.  The title of the message requesting delete confirmation
-              text: 'This action cannot be reversed!', // The inner content of text to be displayed
-              id, // The id to be passed to the delete function when confirmed (optional)
-              confirmAction: () => remove[type](id), // Function called when action is confirmed
-              confirmBtnColor: 'red', // String value.  Accepts color of confirmation button.
-              icon: 'exclamation', // String value or null.  Icon next to the title
-              btnIcon: 'trash alternate', // String value or null.  Icon inside the confirmation button
-              btnText: 'Yes, delete', // string value.  Defaults to "yes"
-            });
-          }}
-          content="Delete draft"
+      {({ showConfirm, remove, edit }) => (
+        <Popup
+          content={`Delete this ${type}`}
+          trigger={(
+            <Button
+              color="red"
+              width="4"
+              onClick={(e) => {
+                e.preventDefault();
+                showConfirm({
+                  title: `Are you sure you want to delete this ${type}?`, // REQUIRED.  The title of the message requesting delete confirmation
+                  text: 'This action cannot be reversed!', // The inner content of text to be displayed
+                  id, // The id to be passed to the delete function when confirmed (optional)
+                  confirmAction: async () => {
+                    if (type === 'painting') {
+                      await edit.painting({ artistId: null }, id);
+                    }
+                    return remove[type](id);
+                  }, // Function called when action is confirmed
+                  confirmBtnColor: 'red', // String value.  Accepts color of confirmation button.
+                  icon: 'exclamation', // String value or null.  Icon next to the title
+                  btnIcon: 'trash alternate', // String value or null.  Icon inside the confirmation button
+                  btnText: 'Yes, delete', // string value.  Defaults to "yes"
+                });
+              }}
+              icon={!showText}
+            >
+              {showText ? 'Delete draft' : (
+                <Icon name="trash alternate" />
+              )}
+            </Button>
+)}
         />
       )}
     </Consumer>
@@ -33,4 +47,10 @@ export function DeleteButton({ id, type }) {
 DeleteButton.propTypes = {
   id: PropTypes.number.isRequired,
   type: PropTypes.oneOf(['artist', 'painting', 'customer', 'employee']).isRequired,
+  showText: PropTypes.bool,
+};
+
+
+DeleteButton.defaultProps = {
+  showText: true,
 };
