2fca947387d72bb4ba91f83e95138e8e77ee1d89
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-07-01 13:19:53 -0500

    Added store account balance.

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,14 +1,25 @@
-import React, { useState } from 'react';
-import { Tab } from 'semantic-ui-react';
+import React, { useState, useEffect } from 'react';
+import {
+  Tab, Header, Table,
+} from 'semantic-ui-react';
 import { Consumer } from '../../ContextProvider';
 import { OrderTable } from './OrderTable';
+import { API } from '../../modules/api/API';
 
 
 export function Orders() {
   const [activeTab, setActiveTab] = useState(0);
+  const [storeAccount, setStoreAccount] = useState(null);
+
+  const getStoreAccount = () => API.stores.get().then(setStoreAccount);
+
+
+  useEffect(() => { getStoreAccount(); }, []);
+
 
   const handleChange = (_e, { activeIndex }) => {
     setActiveTab(activeIndex);
+    getStoreAccount();
   };
 
   const panes = [
@@ -50,10 +61,27 @@ export function Orders() {
   ];
 
   return (
-    <Tab
-      activeIndex={activeTab}
-      onTabChange={handleChange}
-      panes={panes}
-    />
+    <>
+      {storeAccount ? (
+        <Table celled unstackable>
+          <Table.Body>
+            <Table.Row>
+              <Table.Cell>
+                <Header as="h4" content="Store account balance: " />
+
+              </Table.Cell>
+              <Table.Cell>
+                <Header as="h4" content={`$${storeAccount.accountBalance}`} />
+              </Table.Cell>
+            </Table.Row>
+          </Table.Body>
+        </Table>
+      ) : null}
+      <Tab
+        activeIndex={activeTab}
+        onTabChange={handleChange}
+        panes={panes}
+      />
+    </>
   );
 }
