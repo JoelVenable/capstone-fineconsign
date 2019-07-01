85220fe2feb71d32b060ec30509f371fdda528ca
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-06-30 20:00:31 -0500

    started basic account view

--- src/components/Account/Account.jsx
+++ src/components/Account/Account.jsx
@@ -1,17 +1,57 @@
 import React from 'react';
-// import { Image, Header } from 'semantic-ui-react';
+import { Segment, Header, Table } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 // import { ArtistProfile } from '../Artists/ArtistProfile';
 
 export function Account({ user }) {
-  return (
-    <>
-  Hello from Accounts
-  Usertype:
-      {' '}
-      {user.userType}
-    </>
+  let data = {};
+  if (user) {
+    if (user.userType === 'artist') {
+      data = user.artist;
+    }
+    if (user.userType === 'customer') {
+      data = user.customer;
+    }
+  }
 
+  return user ? (
+    <Segment.Group>
+      <Segment>
+        <Header content="Account Details" />
+      </Segment>
+      <Segment>
+        <Table columns={2} style={{ width: '50%' }}>
+          <Table.Body>
+            <AccountRow header="Username:" content={user.username} />
+            <AccountRow header="Email:" content={user.email} />
+            <AccountRow
+              header="Address:"
+              content={(
+                <>
+                  <p>{data.address}</p>
+                  <p>{`${data.city}, ${data.state} ${data.zipcode}`}</p>
+                </>
+              )}
+            />
+            <AccountRow header="Account Balance: " content={`$${data.accountBalance}`} />
+          </Table.Body>
+        </Table>
+      </Segment>
+      {user.userType === 'customer' ? (
+        <Segment>
+          <Header content="My Orders" />
+
+        </Segment>
+      ) : null}
+    </Segment.Group>
+
+  ) : (
+    <Segment.Group>
+      <Segment>
+        <Header content="No User found" />
+      </Segment>
+
+    </Segment.Group>
   );
 }
 
@@ -20,4 +60,23 @@ Account.propTypes = {
   user: PropTypes.shape({
     userType: PropTypes.string.isRequired,
   }).isRequired,
+};
+
+
+function AccountRow({ header, content }) {
+  return (
+    <Table.Row>
+      <Table.Cell width={4}>
+        {header}
+      </Table.Cell>
+      <Table.Cell width={6}>
+        {content}
+      </Table.Cell>
+    </Table.Row>
+  );
+}
+
+AccountRow.propTypes = {
+  header: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired,
 };
