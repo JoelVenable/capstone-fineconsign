4fb4a00d35e9f3089fbfdd220319909a01ecf9bf
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-07-01 16:56:33 -0500

    Added transitions for welcome screen.

--- src/components/welcome/GalleryButton.jsx
+++ src/components/welcome/GalleryButton.jsx
@@ -1,40 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
-  Button, Icon, Image, Header,
+  Button, Icon, Image, Header, Transition,
 } from 'semantic-ui-react';
 import { Consumer } from '../../ContextProvider';
 import largeImg from './assets/gallery-lg.jpg';
 
 export function GalleryButton() {
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => { setTimeout(() => setVisible(true), 600); }, []);
+
   return (
 
     <Consumer>
       {({ history, showLogin }) => (
-        <div className="loginRegisterBox">
-          <Header className="loginRegisterBox--header">Go to Gallery</Header>
-          <Image
-            src={largeImg}
-            onClick={() => history.push('/gallery')}
-            fluid
-            style={{
-              cursor: 'pointer',
-              borderRadius: '6px',
-              marginBottom: '2rem',
-            }}
-          />
+        <Transition visible={visible} animation="scale" duration={800}>
+          <div className="loginRegisterBox">
+            <Header className="loginRegisterBox--header">Go to Gallery</Header>
+            <Image
+              src={largeImg}
+              onClick={() => history.push('/gallery')}
+              fluid
+              style={{
+                cursor: 'pointer',
+                borderRadius: '6px',
+                marginBottom: '2rem',
+              }}
+            />
 
-          <Button
-            fluid
-            icon
-            labelPosition="left"
-            onClick={showLogin}
-          >
-            <Icon name="sign-in" />
+            <Button
+              fluid
+              icon
+              labelPosition="left"
+              onClick={showLogin}
+            >
+              <Icon name="sign-in" />
 Log in
-          </Button>
+            </Button>
 
-        </div>
-
+          </div>
+        </Transition>
       )
   }
     </Consumer>
--- src/components/welcome/Title.jsx
+++ src/components/welcome/Title.jsx
@@ -1,34 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { Header } from 'semantic-ui-react';
+import { Header, Transition } from 'semantic-ui-react';
+
+const style = {
+  mobile: {
+    fontFamily: 'Euphoria Script, cursive',
+    fontSize: '4rem',
+    fontWeight: '400',
+    textAlign: 'center',
+  },
+  desktop: {
+    fontFamily: 'Euphoria Script, cursive',
+    fontSize: '8rem',
+    fontWeight: '400',
+    textAlign: 'center',
+  },
+};
 
 
 export function Title({ position, children, history }) {
+  const [visible, setVisible] = useState(false);
   const titleClassName = (position === 'hero') ? 'title--hero' : 'title--navbar';
   let handleClick = null;
   if (position === 'navbar') {
     handleClick = () => history.push('/gallery');
   }
 
+  useEffect(() => { setTimeout(() => setVisible(true), 200); }, []);
+
   return (
     <>
       <div className={titleClassName}>
         <div>
-          <Header
-            as="h1"
-            style={{
-              fontFamily: 'Euphoria Script, cursive',
-              fontSize: '4rem',
-              fontWeight: '400',
-              textAlign: 'center',
-              '@media (minWidth: 480px)': {
-                fontSize: '6rem',
-              },
-            }}
-            onClick={handleClick}
-            onKeyUp={handleClick}
-            content="Fine Consign"
-          />
+          <Transition visible={visible} animation="scale" duration={800}>
+            <Header
+              as="h1"
+              style={style.desktop}
+              onClick={handleClick}
+              onKeyUp={handleClick}
+              content="Fine Consign"
+            />
+          </Transition>
         </div>
         {children}
       </div>
